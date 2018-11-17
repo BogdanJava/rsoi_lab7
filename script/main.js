50b257ab4e5e091bfe0c650ce9bbf68d47483620
Bahdan Shyshkin <bogdanshishkin1998@gmail.com>
2018-11-17 17:55:47 +0300

    add suffix button

--- script/main.js
+++ script/main.js
@@ -3,8 +3,41 @@ $(document).ready(() => {
     let surnamesMap = getSurnamesMap();
     moveSurnames(surnamesMap, removeMovedSurnames);
   });
+  $("#addSuffixButton").click(() => {
+    addSuffixes();
+  });
+
   initModals();
 });
+
+function addSuffixes() {
+  $("#surnameGroupList .level2").each((index, element) => {
+    let suffix = resolveSuffix(index + 1);
+    $(element)
+      .find("li h3")
+      .each((subitemIndex, header) => {
+        if (!suffixPresents(header.innerText)) {
+          header.innerText += `:${suffix}`;
+        }
+      });
+  });
+}
+
+function suffixPresents(text) {
+  for (let key of Object.keys(suffixes)) {
+    if (text.endsWith(suffixes[key])) return true;
+  }
+  return false;
+}
+
+function resolveSuffix(number) {
+  if (number < 0 || !Number.isInteger(number)) {
+    let message = "Incorrect 'position' value: " + number;
+    throw new Error(message);
+    pushNotification("INTERNAL ERROR", message, NotificationType.DANGER, 3000);
+  }
+  return suffixes[number];
+}
 
 function removeMovedSurnames(surnamesMap) {
   surnamesMap.forEach((groupList, group) => {
@@ -22,13 +55,21 @@ function moveSurnames(surnamesMap, removeSurnames) {
   let removedSurnamesMap = new Map();
   surnamesMap.forEach((groupList, group) => {
     if (group) {
-      $(`#${group}`).append(constructList(groupList));
+      if (!listExists(group)) {
+        $(`#${group}`).append(constructList(groupList));
+      } else {
+        $(`#${group} .level2`).append(constructListItems(groupList));
+      }
     }
     if (groupList.length > 0) {
       removedSurnamesMap.set(group, groupList);
     }
   });
   removeSurnames(removedSurnamesMap);
+}
+
+function listExists(group) {
+  return $(`#${group} .level2`).get().length != 0;
 }
 
 function getSurnamesMap() {
@@ -47,9 +88,37 @@ function getSurnamesMap() {
 }
 
 function constructList(groupList) {
+  let items = constructListItems(groupList);
+  return `<ul class="list sublist level2">${items}</ul>`;
+}
+
+function constructListItems(groupList) {
   let items = "";
   groupList.forEach(listItem => {
     items += `<li><h3>${listItem}</h3></li>`;
   });
-  return `<ul class="list sublist level2">${items}</ul>`;
+  return items;
 }
+
+let suffixes = {
+  "1": "first",
+  "2": "second",
+  "3": "third",
+  "4": "fourth",
+  "5": "fifth",
+  "6": "sixth",
+  "7": "seventh",
+  "8": "eighth",
+  "9": "nineth",
+  "10": "tenth",
+  "11": "eleventh",
+  "12": "twelfth",
+  "13": "thirteenth",
+  "14": "fourteenth",
+  "15": "fifteenth",
+  "16": "sixteenth",
+  "17": "seventeenth",
+  "18": "eighteenth",
+  "19": "nineteenth",
+  "20": "twentieth"
+};
